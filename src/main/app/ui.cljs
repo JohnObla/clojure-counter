bed0b2b86769cdcac5361ef86e53c6b4e576a0e0
John Obla <jo.github@outlook.com>
2021-12-30 19:09:17 +0100

    allow ui-counter to render

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -3,10 +3,16 @@
   [com.fulcrologic.fulcro.components :as compo :refer [defsc]]
   [com.fulcrologic.fulcro.dom :as dom]))
 
-(defsc Counter [_ _]
-  (dom/p "Counter"))
+(defsc Counter [_ {:counter/keys [count]}]
+  {:query [:counter/id :counter/count]
+   :ident :counter/id
+   :initial-state (fn [{:keys [id count]}] {:counter/id id :counter/count count})}
+  (dom/p (str "You have clicked " count " times")
+         (dom/button "Click Me!")))
 
 (def ui-counter (compo/factory Counter))
 
-(defsc Root [_ _]
-  (dom/div "ui-counter"))
+(defsc Root [_ {:keys [counters]}]
+  {:query [{:counters (compo/get-query Counter)}]
+   :initial-state  {:counters {:id 1 :count 0}}}
+  (dom/div (ui-counter counters)))
