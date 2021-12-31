5d7365b5892d81c9c9208c68a659cf64c3063e72
John Obla <jo.github@outlook.com>
2021-12-31 13:01:12 +0100

    get inc-count to work

--- src/main/app/mutations.cljs
+++ src/main/app/mutations.cljs
@@ -2,15 +2,6 @@
   (:require
    [com.fulcrologic.fulcro.mutations :refer [defmutation]]))
 
-(defmutation inc-count
-  "Increment count by one"
-  [{id :counter/id}]
+(defmutation inc-count [{:counter/keys [id]}]
   (action [{:keys [state]}]
           (swap! state update-in [:counter/id id :counter/count] inc)))
-
-(comment (def some-map {:counters {1 {:counter/id 1
-                                      :counter/count 0}}}))
-
-(comment (update-in some-map [:counters 1 :counter/count] inc))
-
-(comment (:counters some-map))
--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -1,18 +1,24 @@
 (ns app.ui
   (:require
    [com.fulcrologic.fulcro.components :as compo :refer [defsc]]
-   [com.fulcrologic.fulcro.dom :as dom]))
+   [com.fulcrologic.fulcro.dom :as dom]
+   [app.mutations :as api]))
 
-(defsc Counter [_ {:counter/keys [count]}]
-  {:query [:counter/id :counter/count]
-   :ident :counter/id
-   :initial-state (fn [{:keys [id count]}] {:counter/id id :counter/count count})}
-  (dom/p (str "You have clicked " count " times")
-         (dom/button "Click Me!")))
-
+(defsc Counter [_ {:counter/keys [id count]} {:keys [onInc]}]
+  {:ident :counter/id
+   :query [:counter/id :counter/count]
+   :initial-state {:counter/id :param/id
+                   :counter/count :param/count}}
+  (dom/p (str "You have clicked " count " times   ")
+         (dom/button {:onClick #(onInc id)} "Click Me!")))
 (def ui-counter (compo/factory Counter))
 
-(defsc Root [_ {:keys [counter]}]
-  {:query [{:counter (compo/get-query Counter)}]
-   :initial-state (fn [_] {:counter (compo/get-initial-state Counter {:id 1 :count 0})})}
-  (dom/div (ui-counter counter)))
+(defsc Root [this {:root/keys [counter]}]
+  {:query [{:root/counter (compo/get-query Counter)}]
+   :initial-state {:root/counter {:id 1 :count 0}}}
+  (let [inc-count (fn [counter-id] (compo/transact! this [(api/inc-count {:counter/id counter-id})]))]
+    (dom/div
+     (ui-counter (compo/computed counter {:onInc inc-count})))))
+
+(comment (compo/get-query Root))
+(comment (compo/get-initial-state Counter {:id 2 :count 55}))
